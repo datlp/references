dedf3afac90acf841bf45280a78504fd59400ee9
Dat <38286831+PeterDatLe@users.noreply.github.com>
2022-08-13 11:07:06 +0700

    Create code-jav.js

---
+++ timfshare/code-jav.js
@@ -0,0 +1,638 @@
+REG_MOVIE_EXTENSIONS =
+  /[.](avi|mpg|mpe|mpeg|asf|wmv|mov|qt|rm|mp4|flv|m4v|webm|ogv|ogg|mkv|ts|tsv)/;
+PAGESIZE = 10;
+CODES = `FSDSS
+DLDSS
+SDDE
+SDAB
+MOGI
+KIRE
+SSIS
+YST
+YSN
+WZEN
+EKW
+VENX
+VEMA
+VEC
+SAN
+ROYD
+ROE
+REXD
+QRDA
+OPPW
+NTRD
+NSFS
+NEWM
+MOND
+MKMP
+MISM
+MADM
+LZDM
+JYMA
+JSOP
+HUNTB
+HUNBL
+HOMA
+HMN
+HEYZO
+GMEM
+DASS
+CMV
+CJOD
+CEMD
+CEFD
+BONY
+BLB
+BDA
+BAB
+AVSA
+APNS
+APAA
+ALDN
+AARM
+JKSR
+ITSR
+BDSR
+URE
+SKMJ
+OAE
+IBW
+AOZ
+SW
+SVDVD
+STARS
+SDTH
+SDNM
+SDMF
+SDJS
+REBD
+RCTD
+NHDTB
+MGOLD
+MESU
+IENF
+HTHD
+HBAD
+GS
+FERA
+DANDY
+ARSO
+AKDL
+VNDS
+NXG
+NACR
+KIR
+AMBI
+VAGU
+SYKH
+SUJI
+SORA
+SOAN
+RVG
+PRED
+PPPE
+PBD
+ONSG
+MYBA
+MVSD
+MUDR
+MUCD
+MRHP
+MOPT
+MMKZ
+MKON
+MIZD
+MIRD
+MIFD
+MIDV
+MIAA
+MEYD
+KYMI
+KTKC
+KSBJ
+KAM
+JUFE
+JJBK
+HJMO
+HJBB
+GVH
+GTJ
+FOCS
+EBOD
+DVDMS
+DPMI
+DDOB
+DDFF
+BLK
+BBTU
+BABM
+AQSH
+MXGS
+DOCP
+NOSKN
+VEO
+URKK
+TYSF
+TPPN
+TPIN
+SCOP
+SABA
+RKI
+REAL
+NKKD
+NITR
+NGOD
+NDRA
+NATR
+NASH
+MDBK
+KTRA
+IPX
+IPIT
+HOKS
+GMA
+EKDV
+DVAJ
+DNJR
+DBER
+CMF
+CMC
+CHRV
+CADV
+BOBB
+BBSS
+BBAN
+BAZX
+BACJ
+APAK
+ANCI
+HZGD
+BANK
+ACZD
+UMD
+STSK
+NEO
+MILK
+JRZE
+HONE
+HAWA
+GUN
+FCDSS
+ETQR
+DRPT
+YMDS
+YMDD
+XVSR
+WAAA
+SSPD
+SQTE
+SOAV
+SCR
+RBK
+PKPR
+PKPL
+PKPD
+NNPJ
+MRSS
+MMUS
+MMND
+MARA
+LULU
+KTKZ
+KTB
+JUTA
+JUNY
+JJDA
+JJCC
+IMO
+HKD
+EVIS
+DLPN
+COGM
+CLUB
+CAWD
+BUR
+BIJN
+BF
+AUKG
+ATID
+ANB
+AED
+ADN
+PED
+HODV
+DOKS
+TOEN
+NUKA
+KAAD
+ZOZO
+XRL
+USBA
+UMSO
+SUPA
+SCPX
+OPBD
+OKAX
+MGMQ
+LUNS
+KATU
+JUL
+ILLE
+GIGL
+GENM
+CPSN
+CJOB
+BXX
+BOKD
+BLOR
+AGMX
+MDVHJ
+SGKX
+MCSR
+JUKF
+HUSR
+MTES
+SVOMN
+SUN
+SDMM
+PIYO
+MTALL
+IESP
+FANH
+CVDX
+COSX
+SCD
+PRTD
+NIMA
+KRU
+JUAN
+EYAN
+DFTR
+DDK
+CHCH
+ZMAR
+NACX
+HDKA
+EMOT
+MBDD
+BNST
+YLWN
+XRLE
+TSP
+SQIS
+MDTM
+FFFS
+ANKB
+ZEAA
+XMOM
+MIST
+MHAR
+JURA
+IZM
+IQQQ
+RPIN
+PKPB
+OVG
+MMB
+MDON
+JBD
+FLAV
+EMBZ
+BMW
+ISRD
+SHIND
+FUGA
+CHERD
+GOJU
+GAS
+NUDV
+GODR
+CRNX
+WKD
+T28
+SAIT
+RDVHJ
+EMSK
+DJE
+VOTAN
+STAR
+ZOCM
+TIKB
+MMKS
+GIRL
+AP
+SREX
+SS
+SDMU
+SDAM
+KEED
+CEAD
+VOD
+IDBD
+SHKD
+MEAT
+HMGL
+BKD
+SPRBD
+NUBI
+DASD
+MADV
+DGCEMD
+DKD
+OMHD
+TIKP
+PRMJ
+MXSPS
+ZEX
+PPBD
+MIMK
+MEKO
+FGAN
+SCBB
+URPW
+ROOM
+MMMB
+PFES
+SGSR
+ECB
+SPRD
+KMHRS
+LBDD
+SPZ
+OFKU
+DOKI
+MMYM
+JUE
+AUKS
+BTHA
+KWBD
+ID
+OFJE
+HNDS
+DDHH
+OKSN
+HMDN
+GNAX
+PPPD
+TOMN
+MBYD
+EYS
+PRBYB
+PPMNB
+KDMI
+KAGP
+BHOL
+IPVR
+PAIS
+MDS
+SDFK
+GREDB
+MIDE
+SXAR
+MGMJ
+ZMEN
+BAVC
+PKPT
+FTDSS
+CLDG
+CPDE
+FCDC
+AGAV
+SPRO
+MUKD
+DAVK
+NYKD
+BAHP
+BADA
+DKWT
+OKP
+KUSE
+UDAK
+MGDN
+DMAT
+MSFH
+BACN
+MLSM
+SDHS
+SHIC
+BUZ
+ARM
+SILK
+APKH
+DINM
+AVKH
+NSPS
+HND
+DSS
+DPMX
+SITW
+TEN
+SIS
+OBA
+HUNTA
+HHH
+OPKT
+CESD
+UKM
+TNOZ
+HMJM
+XRW
+HAVD
+TOTTE
+FUFU
+TCD
+KSAT
+GENS
+DGCESD
+AKID
+LZDQ
+KTSG
+AQMB
+HRV
+ATAD
+SSNI
+DOVR
+NBES
+HRS
+VENU
+MMNT
+RBD
+MARAA
+HGOT
+AWD
+ONED
+AVOP
+WANZ
+MBRBA
+HR
+STKO
+ABP
+DV
+HTMS
+HMPD
+NSSTL
+OYC
+SHYN
+CSCT
+MMGH
+GEN
+MGT
+OKB
+GVG
+BAEM
+TKBN
+SDNT
+JUY
+DSVR
+MIX
+SAMA
+OKS
+LOL
+DDT
+PX
+KAWD
+TEK
+MDB
+VOSS
+JUFD
+MIAE
+TIGR
+SNIS
+ATFB
+TAMM
+MIGD
+IPZ
+MRXD
+PGD
+PLA
+RIX
+NHDTA
+VRTM
+DFET
+LID
+ETEMB
+MIAD
+JUX
+SDSI
+IENE
+MXBD
+HNDB
+MILD
+KTDS
+GDTM
+ANTHOLOGY
+MDYD
+GENT
+MRMM
+BELL
+MIMU
+SOE
+MIDD
+ZSD
+MIBD
+NAGE
+BEB
+ZMIN
+IDL
+BID
+PPSD
+SACE
+CRS
+SDMT
+HERY
+BOB
+MINT
+XV`;
+
+CODES = CODES.toLowerCase()
+  .split('\n')
+  .filter((code) => code);
+
+async function recursion(PAGE = 0) {
+  if (PAGE * PAGESIZE > CODES.length) return;
+  arr = [];
+  values = await Promise.all(
+    CODES.slice(PAGE * PAGESIZE, (PAGE + 1) * PAGESIZE).map((item) =>
+      fetch('https://api.timfshare.com/v1/string-query-search?query=' + item, {
+        headers: {
+          accept: '*/*',
+          'accept-language': 'en-US,en;q=0.9',
+          authorization:
+            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiZnNoYXJlIiwidXVpZCI6IjcxZjU1NjFkMTUiLCJ0eXBlIjoicGFydG5lciIsImV4cGlyZXMiOjAsImV4cGlyZSI6MH0.WBWRKbFf7nJ7gDn1rOgENh1_doPc07MNsKwiKCJg40U',
+          'content-type': 'application/json',
+          'sec-ch-ua':
+            '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+        referrer: 'https://timfshare.com/',
+        referrerPolicy: 'strict-origin-when-cross-origin',
+        body: null,
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+      }).then((response) => response.json())
+    )
+  ).then((result) => {
+    result.forEach((child, order) => {
+      code = CODES[order + PAGE * PAGESIZE].toUpperCase();
+      child.data.forEach(({ id, name }) => {
+        try {
+          const text = name.toLowerCase();
+          if (REG_MOVIE_EXTENSIONS.test(text)) {
+            codeExtracted = text.match(
+              new RegExp(`${code}(-| |_|)[0-9]{2,}`, 'gi')
+            );
+
+            if (Array.isArray(codeExtracted)) {
+              extracted = codeExtracted[0].replaceAll(
+                new RegExp(code + '(-| |_|)', 'gi'),
+                code + '-'
+              );
+
+              arr = [...new Set([...arr, `${extracted}\t${name}\t${id}\t`])];
+            }
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      });
+    });
+  });
+  download('test', arr.sort().reverse().join('\n') + '\n');
+  await recursion(PAGE + 1);
+}
+
+(async () => console.log(recursion(0)))();
+
+function genCode(code = 'star') {
+  text =
+    Array(10)
+      .fill(code)
+      .map(
+        (code, index) =>
+          `${code}-${index} ${code}_${index} ${code}${index} ${code}00${index}`
+      )
+      .join(' ') + ` ${code}0000 ${code}`;
+  return (text || '').split(' ');
+}
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute(
+    'href',
+    'data:text/plain;charset=utf-8,' + encodeURIComponent(text)
+  );
+  element.setAttribute(
+    'download',
+    new Date().getTime() + ' ' + filename + '.txt'
+  );
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
