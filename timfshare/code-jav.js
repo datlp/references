d357d998dc3dc6cfdb7cd8f79c6130a3551f2ae5
Dat <38286831+datlp@users.noreply.github.com>
2022-09-10 18:30:22 +0700

    Update code-jav.js

--- timfshare/code-jav.js
+++ timfshare/code-jav.js
@@ -1,63 +1,73 @@
 REG_MOVIE_EXTENSIONS =
-  /[.](avi|mpg|mpe|mpeg|asf|wmv|mov|qt|rm|mp4|flv|m4v|webm|ogv|ogg|mkv|ts|tsv)/;
+  /[.](AVI|MPG|MPE|MPEG|ASF|WMV|MOV|QT|RM|MP4|FLV|M4V|WEBM|OGV|OGG|MKV|TS|TSV)/;
 PAGESIZE = 10;
-CODES = `567BEAF	BEAF
-546EROF	EROF
-300NTK	NTK
-300MIUM	MIUM
-300MAAN	MAAN
-200GANA	GANA`
-CODES = CODES.toLowerCase()
-  .split('\n')
-  .filter((code) => code);
-
-async function recursion(PAGE = 0) {
+async function recursion(PAGE = 0, CODES = []) {
   if (PAGE * PAGESIZE > CODES.length) return;
   arr = [];
   values = await Promise.all(
     CODES.slice(PAGE * PAGESIZE, (PAGE + 1) * PAGESIZE).map((item) =>
-      fetch('https://api.timfshare.com/v1/string-query-search?query=' + item.split('\t')[0], {
-        headers: {
-          accept: '*/*',
-          'accept-language': 'en-US,en;q=0.9',
-          authorization:
-            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiZnNoYXJlIiwidXVpZCI6IjcxZjU1NjFkMTUiLCJ0eXBlIjoicGFydG5lciIsImV4cGlyZXMiOjAsImV4cGlyZSI6MH0.WBWRKbFf7nJ7gDn1rOgENh1_doPc07MNsKwiKCJg40U',
-          'content-type': 'application/json',
-          'sec-ch-ua':
-            '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
-          'sec-ch-ua-mobile': '?0',
-          'sec-ch-ua-platform': '"Windows"',
-          'sec-fetch-dest': 'empty',
-          'sec-fetch-mode': 'cors',
-          'sec-fetch-site': 'same-site',
-        },
-        referrer: 'https://timfshare.com/',
-        referrerPolicy: 'strict-origin-when-cross-origin',
-        body: null,
-        method: 'POST',
-        mode: 'cors',
-        credentials: 'include',
-      }).then((response) => response.json())
+      fetch(
+        "https://api.timfshare.com/v1/string-query-search?query=" + item.search,
+        {
+          headers: {
+            accept: "*/*",
+            "accept-language": "en-US,en;q=0.9",
+            authorization:
+              "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiZnNoYXJlIiwidXVpZCI6IjcxZjU1NjFkMTUiLCJ0eXBlIjoicGFydG5lciIsImV4cGlyZXMiOjAsImV4cGlyZSI6MH0.WBWRKbFf7nJ7gDn1rOgENh1_doPc07MNsKwiKCJg40U",
+            "content-type": "application/json",
+            "sec-ch-ua":
+              '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Windows"',
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-site",
+          },
+          referrer: "https://timfshare.com/",
+          referrerPolicy: "strict-origin-when-cross-origin",
+          body: null,
+          method: "POST",
+          mode: "cors",
+          credentials: "include",
+        }
+      ).then((response) => response.json())
     )
   ).then((result) => {
     result.forEach((child, order) => {
-    input = CODES[order + PAGE * PAGESIZE].toUpperCase().split('\t');
-    searchKey = input?.[0] || input
-      code =  input?.[1]|| input
+      input = CODES[order + PAGE * PAGESIZE];
+      searchKey = input.search;
+      number = input.number;
+      code = input.code;
+      console.log(searchKey, number, code);
       child.data.forEach(({ id, name }) => {
         try {
-          const text = name.toLowerCase();
+          const text = name
+            .toUpperCase()
+            ?.replaceAll(
+              new RegExp(`${searchKey}(-| |_|N|)[0]{1,}[0-9]{${number}}`, "gi"),
+              (found) =>
+                found.replace(
+                  new RegExp(`(-| |_|N|)[0]{1,}[0-9]{${number},}`),
+                  (founded) => founded.slice(-1 * parseInt(number))
+                )
+            );
+
           if (REG_MOVIE_EXTENSIONS.test(text)) {
             codeExtracted = text.match(
-              new RegExp(`${searchKey}(-| |_|)[0-9]{2,}`, 'gi')
+              new RegExp(`${searchKey}(-| |_|)[0-9]{${number},}`, "gi")
             );
             if (Array.isArray(codeExtracted)) {
               extracted = codeExtracted[0].replaceAll(
-                new RegExp(searchKey + '(-| |_|)', 'gi'),
-                code + '-'
+                new RegExp(searchKey + "(-| |_|)", "gi"),
+                code + "-"
               );
 
-              arr = [...new Set([...arr, `${extracted.replace()}\t${name}\t${id}\t`])];
+              arr = [
+                ...new Set([
+                  ...arr,
+                  `${extracted.replace()}\t${name}\t${id}`,
+                ]),
+              ];
             }
           }
         } catch (e) {
@@ -66,35 +76,27 @@ async function recursion(PAGE = 0) {
       });
     });
   });
-  download('test', arr.join('\n') + '\n');
-  await recursion(PAGE + 1);
+  download(CODES[PAGE * PAGESIZE].search, arr.join("\n") + "\n");
+  await recursion(PAGE + 1, CODES);
 }
 
-(async () => console.log(recursion(0)))();
-
-function genCode(code = 'star') {
-  text =
-    Array(10)
-      .fill(code)
-      .map(
-        (code, index) =>
-          `${code}-${index} ${code}_${index} ${code}${index} ${code}00${index}`
-      )
-      .join(' ') + ` ${code}0000 ${code}`;
-  return (text || '').split(' ');
-}
-
+(async () => {
+  CODES = await fetch(
+    "https://script.google.com/macros/s/AKfycby6VQCtXEo9ZRN_aUIf85iqmKRv1KvwlOlnxiYSMA75oU-7IwjT4TVzyrnxdC77tDgPhQ/exec?_uniqueField=id&is_get_all=true&name_sheet=CODES&id_spreadsheet=1j96GzNo0qvlhnBHkFlTjdJxeqmcA0VDwADS2EOub1no"
+  ).then((res) => res.json());
+  console.log(recursion(0, CODES.slice(0,25)));
+})();
 function download(filename, text) {
-  var element = document.createElement('a');
+  var element = document.createElement("a");
   element.setAttribute(
-    'href',
-    'data:text/plain;charset=utf-8,' + encodeURIComponent(text)
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
   );
   element.setAttribute(
-    'download',
-    new Date().getTime() + ' ' + filename + '.txt'
+    "download",
+    new Date().getTime() + " " + filename + ".txt"
   );
-  element.style.display = 'none';
+  element.style.display = "none";
   document.body.appendChild(element);
   element.click();
   document.body.removeChild(element);
