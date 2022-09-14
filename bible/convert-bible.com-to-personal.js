e0f99df5a52a811832fd5c56098283df6969379e
Dat <38286831+datlp@users.noreply.github.com>
2022-09-14 21:50:44 +0700

    Update convert-bible.com-to-personal.js

--- bible/convert-bible.com-to-personal.js
+++ bible/convert-bible.com-to-personal.js
@@ -1,35 +1,18 @@
-index  = 1;
-function recursion () {
-    books = [...new Set([...document.querySelectorAll('.book')].map(node=>[...node.classList].map(item=>'.'+item).join('')))];
-    if(books.length===0) return ;
-document.querySelectorAll(books[0]).forEach(bookNode=>{
-     bookNode.querySelectorAll('.chapter>.label').forEach(node=>{node.innerText = `<strong class="label" data-level="chapter" id="${node.parentNode.dataset.usfm}">Chương ${node.innerText}</strong>`})
-bookNode.querySelectorAll('.chapter .heading').forEach(node=>{node.innerText = `<strong class="heading" data-level="chapter">${node.innerText}</strong>`})
-bookNode.querySelectorAll('.chapter .verse > .label').forEach(node=>{node.innerText = `<sup class="label" data-level="verse" id="${node.parentNode.dataset.usfm}">${node.innerText}</sup>`})
-bookNode.querySelectorAll('.chapter .verse').forEach(verse=>{
-    verse.querySelectorAll('.verse .content').forEach(content=>{
-        content.innerText = `<span class="content" data-level="verse" data-usfm="${verse.dataset.usfm}">${content.innerText}</span>`
-    })
-})
-});
-txt = `<div class="book" id="${books[0].match(/bk.*/)?.[0]}">` +[...document.querySelectorAll(books[0])].map(node=>node.innerText).join('').replaceAll('\n','<br>') + `</div>`;
-    document.querySelectorAll(books[0]).forEach(node=>node.remove())
-    download(`000${index}`.slice(-3)+'-'+books[0]+'.html',txt)
-    index++;
-recursion()
-}
-recursion()
-function download(filename, text) {
-    if(!filename) return;
-    var pom = document.createElement('a');
-    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    pom.setAttribute('download', filename);
-
-    if (document.createEvent) {
-        var event = document.createEvent('MouseEvents');
-        event.initEvent('click', true, true);
-        pom.dispatchEvent(event);
+document.querySelectorAll('.chapter .p .verse:last-child .content:last-child').forEach(node=>node.dataset.break=true);
+document.querySelectorAll('.chapter .p .verse .content').forEach(node=>node.dataset.uuid=`${node.parentNode.dataset.usfm}`);
+document.querySelectorAll('.chapter .heading').forEach((node,order)=>node.dataset.uuid=`heading-${order}`);
+obj = {};
+document.querySelectorAll('.chapter .heading,.chapter .verse .content').forEach((node,order)=>{
+    isBreak = node.dataset.break)
+    if(obj[node.dataset.uuid]) {
+        obj[node.dataset.uuid].content += ` ${node.innerText}`;
+        obj[node.dataset.uuid].isBreak = isBreak;
     } else {
-        pom.click();
+        obj[node.dataset.uuid] = {
+            content:node.innerText,
+            order,
+            isBreak
+        }
     }
-}
+})
+JSON.stringify(obj)
