c591b29f0d1f206bd5e657cb070cb368023b2bc9
Dat <38286831+datlp@users.noreply.github.com>
2022-09-24 07:44:50 +0700

    Update code-jav.js

--- timfshare/code-jav.js
+++ timfshare/code-jav.js
@@ -23,7 +23,7 @@ apiHeaders = {
 (async () => {
   structures  = (await fetch("https://script.google.com/macros/s/AKfycby6VQCtXEo9ZRN_aUIf85iqmKRv1KvwlOlnxiYSMA75oU-7IwjT4TVzyrnxdC77tDgPhQ/exec?_uniqueField=id&is_get_all=true&name_sheet=CodeDeep&id_spreadsheet=1j96GzNo0qvlhnBHkFlTjdJxeqmcA0VDwADS2EOub1no")
   .then((res) => res.json())).filter((_)=>!_.prevent && _.id);
-  //structures = [{ code: "hunt", digits: 3, id: "ipx-•••", len: 3, status: 1 }];
+  //structures = [{ code: "heyzo", digits: 4, id: "ipx-•••", len: 3, status: 1 }];
     console.log(structures)
   recursion(structures, 0);
 })();
@@ -32,32 +32,36 @@ function recursion(structures, __index) {
   let { code, digits } = structures[__index] || {};
   if (!code || !digits) return;
   code = code?.toLowerCase()?.replaceAll(' ','');
-  filter = new RegExp(`${code}[.][0-9]{${digits}}[.]`);
+  filter = new RegExp(`[.]${code}[.][0-9]{${digits}}[.]`);
   fetchMovies(code, (data = []) => {
     standard = data.map((result = {}) => {
       const { name = "" } = result;
       _name = name
         .toLowerCase()
         .replaceAll(/[^0-9a-z]/g, ".")
+        ?.replaceAll(`${code}.hd`, code)
+        ?.replaceAll(`${code}hd`, code)
         ?.replaceAll(`${code}00`, code)
         ?.replaceAll(/[^0-9a-z]/g, ".")
         ?.replaceAll(/[0-9]{2,}/g, ".$&")
         ?.replaceAll(/[.]{1,}/g, ".");
-      if (!_name.match(EXTENSIONS)?.[0]) return result;
+        _name="." + _name;
+      if (!_name.match(EXTENSIONS)?.[0]) return {_name,...result};
       __code = _name
         .match(filter)?.[0]
         ?.replaceAll(/[.]{1,}/g, "-")
         ?.toUpperCase()
-        ?.slice(0,-1);
+        ?.slice(1,-1);
 
-      return __code ? { __code, ...result } : result;
+      return __code ? { __code, _name,...result } : {_name,...result};
     });
     
     filterMovies = standard
       .filter((item = {}) => item.__code)
       .sort((a, b) => (a.__code < b.__code ? 1 : -1));
-      
+     
     console.log(`${((__index/structures.length)*100).toFixed(0)} % - data: ${data.length} - matched: ${filterMovies.length} - ${code} - ${digits} `);
+    console.log(filter," ",standard);
     fileContent = filterMovies
       .map(({ id, __code,name }) => `${__code}\t${id}\t${name}`)
       .join("\n");
@@ -120,3 +124,4 @@ function download(filename, text) {
   element.click();
   document.body.removeChild(element);
 }
+// https://github.com/datlp/references/blob/main/timfshare/code-jav.js
